a9776b6af96d41d0328acff5fe3e60fe2f7542e5
Oliver Chalupka <oliverchalupka225@gmail.com>
2024-07-24 10:46:07 +0200

    vytvorenie základných súborou a prvé uloženie do gitu

---
+++ temp/cache/latte/@layout.latte--8fe3d07caf.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+use Latte\Runtime as LR;
+
+/** source: C:\wamp64\www\Nette-projekt4\app\UI/@layout.latte */
+final class Template_8fe3d07caf extends Latte\Runtime\Template
+{
+	public const Source = 'C:\\wamp64\\www\\Nette-projekt4\\app\\UI/@layout.latte';
+
+	public const Blocks = [
+		['scripts' => 'blockScripts'],
+	];
+
+
+	public function main(array $ʟ_args): void
+	{
+		extract($ʟ_args);
+		unset($ʟ_args);
+
+		if ($this->global->snippetDriver?->renderSnippets($this->blocks[self::LayerSnippet], $this->params)) {
+			return;
+		}
+
+		echo '<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+
+	<title>';
+		if ($this->hasBlock('title')) /* line 7 */ {
+			$this->renderBlock('title', [], function ($s, $type) {
+				$ʟ_fi = new LR\FilterInfo($type);
+				return LR\Filters::convertTo($ʟ_fi, 'html', $this->filters->filterContent('stripHtml', $ʟ_fi, $s));
+			}) /* line 7 */;
+			echo ' | ';
+		}
+		echo 'Nette Web</title>
+</head>
+
+<body>
+	<header>
+
+	</header>
+	<main>
+';
+		foreach ($flashes as $flash) /* line 15 */ {
+			echo '		<div';
+			echo ($ʟ_tmp = array_filter(['flash', $flash->type])) ? ' class="' . LR\Filters::escapeHtmlAttr(implode(" ", array_unique($ʟ_tmp))) . '"' : "" /* line 15 */;
+			echo '>';
+			echo LR\Filters::escapeHtmlText($flash->message) /* line 15 */;
+			echo '</div>
+';
+
+		}
+
+		echo "\n";
+		$this->renderBlock('content', [], 'html') /* line 17 */;
+		echo '	</main>
+';
+		$this->renderBlock('scripts', get_defined_vars()) /* line 19 */;
+		echo '</body>
+</html>
+';
+	}
+
+
+	public function prepare(): array
+	{
+		extract($this->params);
+
+		if (!$this->getReferringTemplate() || $this->getReferenceType() === 'extends') {
+			foreach (array_intersect_key(['flash' => '15'], $this->params) as $ʟ_v => $ʟ_l) {
+				trigger_error("Variable \$$ʟ_v overwritten in foreach on line $ʟ_l");
+			}
+		}
+		return get_defined_vars();
+	}
+
+
+	/** {block scripts} on line 19 */
+	public function blockScripts(array $ʟ_args): void
+	{
+		echo '	<script src="https://unpkg.com/nette-forms@3/src/assets/netteForms.js"></script>
+';
+	}
+}
---
+++ temp/cache/nette.search/65167f8cc078e00fb88e6bc87af50ce4.php
@@ -0,0 +1,42 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Bootstrap' => 
+    array (
+      0 => 'C:\\wamp64\\www\\Nette-projekt4\\app\\Bootstrap.php',
+      1 => 1721799171,
+    ),
+    'App\\Core\\RouterFactory' => 
+    array (
+      0 => 'C:\\wamp64\\www\\Nette-projekt4\\app\\Core\\RouterFactory.php',
+      1 => 1721799171,
+    ),
+    'App\\UI\\Accessory\\LatteExtension' => 
+    array (
+      0 => 'C:\\wamp64\\www\\Nette-projekt4\\app\\UI\\Accessory\\LatteExtension.php',
+      1 => 1721799171,
+    ),
+    'App\\UI\\Error\\Error4xx\\Error4xxPresenter' => 
+    array (
+      0 => 'C:\\wamp64\\www\\Nette-projekt4\\app\\UI\\Error\\Error4xx\\Error4xxPresenter.php',
+      1 => 1721799172,
+    ),
+    'App\\UI\\Error\\Error5xx\\Error5xxPresenter' => 
+    array (
+      0 => 'C:\\wamp64\\www\\Nette-projekt4\\app\\UI\\Error\\Error5xx\\Error5xxPresenter.php',
+      1 => 1721799172,
+    ),
+    'App\\UI\\Home\\HomePresenter' => 
+    array (
+      0 => 'C:\\wamp64\\www\\Nette-projekt4\\app\\UI\\Home\\HomePresenter.php',
+      1 => 1721799172,
+    ),
+  ),
+  1 => 
+  array (
+  ),
+  2 => 
+  array (
+  ),
+);
