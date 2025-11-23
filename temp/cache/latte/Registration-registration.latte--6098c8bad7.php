8152ccca976ee5b6004d18928f967b984d24aeca
Oliver Chalupka <oliverchalupka225@gmail.com>
2025-11-23 09:15:29 +0100

    Upravenie linku na aktuálne  používaný link pre stánku so spetnou vezbou online. totiš subdoménu neviem poriadne rozbehnúť.

---
+++ temp/cache/latte/Registration-registration.latte--6098c8bad7.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+use Latte\Runtime as LR;
+
+/** source: C:\wamp64\www\spetna-vezba\app\UI\Registration/registration.latte */
+final class Template_6098c8bad7 extends Latte\Runtime\Template
+{
+	public const Source = 'C:\\wamp64\\www\\spetna-vezba\\app\\UI\\Registration/registration.latte';
+
+	public const Blocks = [
+		['content' => 'blockContent'],
+	];
+
+
+	public function main(array $ʟ_args): void
+	{
+		extract($ʟ_args);
+		unset($ʟ_args);
+
+		if ($this->global->snippetDriver?->renderSnippets($this->blocks[self::LayerSnippet], $this->params)) {
+			return;
+		}
+
+		$this->renderBlock('content', get_defined_vars()) /* line 1 */;
+	}
+
+
+	/** {block content} on line 1 */
+	public function blockContent(array $ʟ_args): void
+	{
+		extract($this->params);
+		extract($ʟ_args);
+		unset($ʟ_args);
+
+		echo '<div class="form-container">
+    <h1>Registracia</h1>
+';
+		$ʟ_tmp = $this->global->uiControl->getComponent('registrationForm');
+		if ($ʟ_tmp instanceof Nette\Application\UI\Renderable) $ʟ_tmp->redrawControl(null, false);
+		$ʟ_tmp->render() /* line 4 */;
+
+		echo '</div>
+';
+	}
+}
